5caac61bdb950dd1ce49714c198cb5523f8492fa
mkruk <kruczy@gmail.com>
2012-03-23 18:09:40 +0100

    moved to using sbt

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "scala.bagofcode.org"
+
+version := "0.1"
+
+scalaVersion := "2.9.1"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.7.1" % "test"
--- src/test/scala/org/bagofcode/scala/test/cache/CacheVarTest.scala
+++ src/test/scala/org/bagofcode/scala/test/cache/CacheVarTest.scala
@@ -1,16 +1,16 @@
 package org.bagofcode.scala.test.cache
 
-import org.junit.Test
 import org.bagofcode.scala.cache._
+import org.scalatest.FunSuite
 
-class CacheVarTest {
-	@Test def value = {
+class CacheVarSpec extends FunSuite {
+	test("returns the functions value") {
 	  var cache = new CacheVar("test",5000)
 	  assert(cache.get == "test")
 	}
 	
-	@Test def expired = {
-	  var cache = new CacheVar("test",1000)
+	test("is expired properly") {
+	  var cache = new CacheVar("test", 1000)
 	  assert(cache.expired)
 	  assert(cache.get == "test")
 	  assert(!cache.expired)
@@ -19,10 +19,10 @@ class CacheVarTest {
 	  assert(cache.get == "test")
 	}
 	
-	@Test def reset = {
-	  var cache = new CacheVar("test",5000)
-	  assert(cache.get == "test")
+	test("reset works as intended") {
+	  var cache = new CacheVar("test", 5000)
+	  (cache.get == "test")
 	  cache.reset
-	  assert(cache.expired)
+	  (cache.expired)
 	}
 }

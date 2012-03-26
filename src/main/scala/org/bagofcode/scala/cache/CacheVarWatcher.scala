21f2dbf06d0b19c0a1050ea933e86ba92d42d037
mkruk <kruczy@gmail.com>
2012-03-26 21:32:05 +0200

    changed TreeMap to PriorityQueue/fixes

--- src/main/scala/org/bagofcode/scala/cache/CacheVarWatcher.scala
+++ src/main/scala/org/bagofcode/scala/cache/CacheVarWatcher.scala
@@ -1,38 +1,49 @@
 package org.bagofcode.scala.cache
 
 import scala.actors.Actor
-import scala.actors.Actor.State.Terminated
 import scala.actors.TIMEOUT
 import scala.ref.WeakReference
-import java.lang.System.currentTimeMillis
+import java.lang.System.{currentTimeMillis => millis}
+import scala.collection.mutable.PriorityQueue
 
 object CacheVarWatcher extends Actor {
+  type T = (Long, WeakReference[CacheVar[_]])
 
-  private val vars = new java.util.TreeMap[Long, WeakReference[CacheVar[_]]]
+  implicit object VarsOrdering extends Ordering[T] {
+    override def compare(x: T, y: T) = Ordering.Long.compare(x._1,y._1)
+  }
 
-  private def timeout: Long = if (vars.isEmpty()) 60000
+  private val vars = new PriorityQueue[T]
+
+  private def timeout: Long = if (vars.size == 0) 60000
   else {
-    var firtstEntry = vars.firstEntry
-    firtstEntry.getValue.get match {
-      case Some(v) => { var ret:Long = 0
-        v.synchronized {
-          if (v.expired) vars.remove(firtstEntry.getKey)
-          else ret = v.expiresOn - currentTimeMillis
+    var top = vars.dequeue
+    if (top._1 < millis)
+      top._2.get match {
+        case Some(v) => {
+          v.synchronized {
+            v.check
+            if (!v.expired) vars.enqueue((v.expiresOn, top._2))
+          }
+          timeout
         }
-      	if(ret != 0) ret else timeout
+        case None => timeout
       }
-      case None => { vars.remove(firtstEntry.getKey); timeout }
+    else {
+      var ret = top._1 - millis
+      if (ret <= 0) ret = 1
+      ret
     }
   }
 
   override def act = loop {
     receiveWithin(timeout) {
       case TIMEOUT =>
-      case Register(v) => vars.put(v.expiresOn, new WeakReference(v))
+      case Register(v) => vars.enqueue((v.expiresOn, new WeakReference(v)))
     }
   }
 
-  def size = vars.size()
+  def size = vars.size
 
   start
 }
